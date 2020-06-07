fb01158edf90849e8a358576e0aa437ee862e74d
Alvaro Negreiros <alvarodenegreiros@gmail.com>
2020-06-07 13:24:02 -0300

    tyopu fix

--- drinkappecommerce/settings.py
+++ drinkappecommerce/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 # update database configuration with $DATABASE_URL
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['defalt'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 #honor the 'X-Forwaded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
