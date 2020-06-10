cedfc78e971e05df0329f3404aa6ba2cf919b2e1
Alvaro Negreiros <alvarodenegreiros@gmail.com>
2020-06-09 21:47:47 -0300

    install django-heroku

--- drinkappecommerce/settings.py
+++ drinkappecommerce/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,6 +137,8 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
 try:
     from .local_settings import *
